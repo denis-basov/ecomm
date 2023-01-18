a311618d47118983cff41c45f9ecb221babfd1f3
Denis Basov <den4er@mail.ru>
2023-01-18 23:40:58 +0300

    parsing form data

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const app = express();
 
+// Request main page, method = GET
 app.get("/", (req, res) => {
+  // Send html to client
   res.send(`
     <div>
-        <form>
-            <input placeholder="email"><br>
-            <input placeholder="password"><br>
-            <input placeholder="password confirmation"><br>
+        <form method="POST">
+            <input name="email" placeholder="email"><br>
+            <input name="password" placeholder="password"><br>
+            <input name="passwordConfirmation" placeholder="password confirmation"><br>
             <button>Sign Up</button>
         </form>
     </div>
   `);
+});
+
+// Request main page, method = POST
+app.post("/", (req, res) => {
+  // If data
+  req.on("data", (data) => {
+    const parsed = data.toString("utf8").split("&"); // recieve data, parse to array of strings
+    const formData = {}; // object for formated data
+    for (let pair of parsed) {
+      // loop through array
+      const [key, value] = pair.split("="); // split every element by '=' on two var
+      formData[key] = value; // add data to final object
+    }
+    console.log(formData);
+  });
+  res.send("Account created");
 });
 
 app.listen(3000, () => {
