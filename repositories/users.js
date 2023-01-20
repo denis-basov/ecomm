f6b974ff4d2558b415a4d92bb7df2d8b59c0c5b3
Denis Basov <den4er@mail.ru>
2023-01-20 19:54:12 +0300

    add random ID to users

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
+/**
+ * class works with users data
+ */
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
@@ -15,14 +19,25 @@ class UsersRepository {
     }
   }
 
-  // get all users
+  /**
+   *
+   * get all users
+   *
+   */
   async getAll() {
     // read file contents, parse, return data
     return JSON.parse(await fs.promises.readFile(this.filename, { encoding: "utf8" }));
   }
 
-  // create new user
+  /**
+   *
+   *  create new user
+   *
+   */
   async create(attrs) {
+    // add random ID to user object
+    attrs.id = this.randomId();
+
     // get array of users from file
     const records = await this.getAll();
 
@@ -30,10 +45,35 @@ class UsersRepository {
     records.push(attrs);
 
     // write the updated array
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    await this.writeAll(records);
   }
-}
 
+  /**
+   *
+   *  write users array to file
+   *
+   */
+  async writeAll(records) {
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+  }
+
+  /**
+   *
+   *  generate random user ID
+   *
+   */
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+} // end of class
+
+/**
+ *
+ *
+ * test function
+ *
+ *
+ */
 const test = async () => {
   const repo = new UsersRepository("users.json");
   await repo.create({ email: "test@test.ru", password: "123456789" });
